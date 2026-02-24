0dd9af770c25f463cddaf2325da88ca59d305749
Abdul-Basit-SF <turkabdulbasit159@gmail.com>
2026-02-24 14:03:35 +0500

    feat: add countUniqueWords method to calculate unique words in a sentence

--- force-app/main/default/lwc/lwcPractoce/lwcPractoce.js
+++ force-app/main/default/lwc/lwcPractoce/lwcPractoce.js
@@ -1,14 +1,4 @@
 import { LightningElement, track } from 'lwc';
 
-export default class LwcPractoce extends LightningElement {
-    @track myDetails = {firstName :'Abdul' , lastName : 'Basit' , age : 22 , city : 'Haripur'};
-    isChanged = false;
-    changeHandle(event){
-        if(this.isChanged){
-            this.myDetails = {firstName :'Abdul' , lastName : 'Basit' , age : 22 , city : 'Haripur'};
-        }else{
-            this.myDetails = {firstName :'John' , lastName : 'Doe' , age : 30 , city : 'New York'};
-        }
-        this.isChanged = !this.isChanged;
-    }
+export default class LwcPractoce extends LightningElement {  
 }
